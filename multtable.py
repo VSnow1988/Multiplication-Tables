d1cd64c1ef99966a60d7f78142fd5f0d9e0daed8
Vincent <vincentsnow@gmx.com>
2017-08-03 16:52:33 -0700

    first commit.

---
+++ multtable.py
@@ -0,0 +1,21 @@
+string = "x "
+for x in range (1, 13):
+    string += " "
+    string += str(x)
+
+print string
+
+string2 ="1 "
+for x in range (1, 13):
+    string2 += " "
+    string2 += str(x*1)
+print string2
+
+string3 ="2 "
+for x in range (1, 13):
+    string3 += " "
+    string3 += str(x*2)
+print string3
+
+
+print "And so on..."
